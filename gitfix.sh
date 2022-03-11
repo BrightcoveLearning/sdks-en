6f372c16fed5beb580d4cb43f19ddac769f5d937
Robert Crooks <rcrooks@brightcove.com>
2022-03-11 10:49:43 -0500

    Update gitfix.sh

--- gitfix.sh
+++ gitfix.sh
@@ -1,8 +1,8 @@
-# git branch -m master main
-# git fetch origin
-# git branch -u origin/main main
-# git remote set-head origin -a
-# echo 'local default branch set to main'
+git branch -m master main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
+echo 'local default branch set to main'
 cd ~/Library/Caches/com.github.GitHubClient
 rm *.*
 echo 'local caches cleaned'
