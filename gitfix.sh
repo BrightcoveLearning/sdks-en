384f17dc80843dfbfcaba1258bbf23a4149c1878
Robert Crooks <rcrooks@brightcove.com>
2022-03-11 09:04:31 -0500

    wip

--- gitfix.sh
+++ gitfix.sh
@@ -3,11 +3,15 @@ git fetch origin
 git branch -u origin/main main
 git remote set-head origin -a
 echo 'local default branch set to main'
-rm ~/Library/Caches
+cd ~/Library/Caches
+rm *.*
 echo 'local caches cleaned'
-rm ~/Library/Application Support
+cd ~/Library/Application Support
+rm *.*
 echo 'local app caches cleaned'
-rm /Library/Caches
+cd /Library/Caches
+rm *.*
 echo 'global caches cleaned'
-rm /Library/Application Support
+cd /Library/Application Support
+rm *.*
 echo 'global app caches cleaned'
