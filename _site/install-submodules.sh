9391acc0c31551c9374064650a9a37f3113c75c8
Robert Crooks <rcrooks@brightcove.com>
2022-05-28 14:49:56 -0400

    https://brightcove.atlassian.net/browse/KNOW-23875

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -7,4 +7,9 @@ cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
 cd ..
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
+cd ..
 git submodule update --init --recursive
